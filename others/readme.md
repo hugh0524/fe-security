d661e663ea37fc4f8b4fc76a293f8033b3eb5fe8
yinhe <yinhe@creditease.cn>
2019-10-18 15:52:37 +0800

    iframe test

---
+++ others/readme.md
@@ -0,0 +1,8 @@
+# 服务启动
+
+http-server ./ -p 8081
+http-server ./ -p 8082
+
+
+# iframe
+
---
+++ others/views/js/csp.js
@@ -0,0 +1,8 @@
+/**
+ * Created by yinhe on 2019/10/18.
+ */
+
+
+setTimeout(function() {
+    document.querySelector("#res").innerHTML="执行了外部js"
+},1000)
